45fe38a79fbefd1abe03a786542b17e565772bbd
i27devopsb6 <i27devops+b6git@gmail.com>
2025-03-12 07:32:18 +0530

    Update firstpipeline.groovy

--- vars/firstpipeline.groovy
+++ vars/firstpipeline.groovy
@@ -1,34 +1,35 @@
-//Jenkins stages
-
-// Import the Calculator class  from the package com.i27academy.builds
 import com.i27academy.builds.Calculator
 
-// Define a function called 'call' that accepts a Map parameter pipelineParams
 def call(Map pipelineParams) {
-    // Create a instance of the Calculator class
+    // an instance of the Calculator class
     Calculator calculator = new Calculator(this)
     pipeline {
-        agent any 
-        environment {
-            // Set an environment variable APPLICATION_NAME 
-            APPLICATION_NAME =  "${pipelineParams.appName}"
+        agent {
+            label 'java-slave'
         }
-
+        environment {
+            APPLICATION_NAME = "${pipelineParams.appName}"
+        }
+        tools {
+            maven 'Maven_3.9.0'
+        }
         stages {
-            stage('Build') {
-                steps {
-                    echo "*************** Building the application *****************"
-                    echo "************** Starting Building ${env.APPLICATION_NAME} application"
-                }
-            }
             stage ('Calculate') {
                 steps {
                     script {
                         echo "Calling Calculator Method from src folder"
-                        echo "***************** Printing the sum of values *****************"
-                        // call the add method 
+                        echo "************ Printing the sum of values ************"
                         calculator.add(2,3)
                     }
+                }
+            }
+            stage ('Build') {
+                steps {
+                    echo "Building the project"
+                    echo "************ In Build Stage for ${env.APPLICATION_NAME}  ************"
+                    // script {
+                    //     calculator.buildApp("${env.APPLICATION_NAME}")
+                    // }
                 }
             }
             stage ('Test') {
@@ -36,26 +37,31 @@ def call(Map pipelineParams) {
                     echo "Testing the project"
                 }
             }
-
             stage ('DevDeploy') {
                 steps {
-                    echo "Deploying the project to dev environment"
+                    echo "Deploying the project to Dev environment"
                 }
             }
-
+            stage ('TestDeploy') {
+                steps {
+                    echo "Deploying the project to Test environment"
+                }
+            }
+            stage ('ProdDeploy') {
+                steps {
+                    echo "Deploying the project to Prod environment"
+                }
+            }
         }
     }
 }
 
+def cal(firstNumber, secondNumber) {
+    return firstNumber + secondNumber
+}
 
+// ${variable}
+// ${env.VARIABLE}
+// ${params.VARIABLE}
+// ${pipelineParams.VARIABLE}
 
-// def newcal(thirdNumber, fourthNumber) {
-//     return thirdNumber+fourthNumber
-// }
-
-
-// Variable placeholder 
-// Local variable : ${variable}
-// Env Variable: ${env.variable}
-// Parameters section: ${params.variable}
-// pipelineParameters: ${pipelineParams.variable}
